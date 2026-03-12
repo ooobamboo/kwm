c2cd9ecb07d1df6333b68aa58cfa1dac4f7b7a68
kewuaa <1692525710@qq.com>
2026-03-12 08:49:26 +0800

    update deck arrange logic, hide all windows below top stack window, regarding: https://github.com/kewuaa/kwm/pull/63#discussion_r2917969354

--- src/kwm/layout/deck.zig
+++ src/kwm/layout/deck.zig
@@ -3,55 +3,61 @@ const Self = @This();
 const std = @import("std");
 const log = std.log.scoped(.deck);
 
-const utils = @import("../utils.zig");
 const Context = @import("../context.zig");
 const Output = @import("../output.zig");
 const Window = @import("../window.zig");
 
+
 inner_gap: i32,
 outer_gap: i32,
+
 
 pub fn arrange(self: *const Self, output: *Output) void {
     log.debug("<{*}> arrange windows in output {*}", .{ self, output });
 
     const context = Context.get();
 
-    var windows: std.ArrayList(*Window) = .empty;
-    defer windows.deinit(utils.allocator);
-
-    var it = context.windows.safeIterator(.forward);
-    while (it.next()) |window| {
-        if (window.is_visible_in(output) and !window.floating)
-            windows.append(utils.allocator, window) catch |err| {
-                log.err("<{*}> append failed: {}", .{ self, err });
-                return;
-            };
-    }
-
-    if (windows.items.len == 0) return;
+    const master = blk: {
+        var it = context.windows.safeIterator(.forward);
+        while (it.next()) |window| {
+            if (window.is_visible_in(output) and !window.floating) {
+                break :blk window;
+            }
+        }
+        return;
+    };
 
     const w = output.exclusive_width() - 2 * self.outer_gap;
     const h = output.exclusive_height() - 2 * self.outer_gap;
 
-    if (windows.items.len == 1) {
-        windows.items[0].unbound_move(self.outer_gap, self.outer_gap);
-        windows.items[0].unbound_resize(w, h);
-        return;
-    }
+    master.unbound_move(self.outer_gap, self.outer_gap);
+    master.unbound_resize(w, h);
 
-    const master_w = @divFloor(w, 2);
-    const stack_w = w - master_w - self.inner_gap;
+    // find top stack window in context.focus_stack
+    {
+        var found_stack_top = false;
+        var it = context.focus_stack.safeIterator(.forward);
+        while (it.next()) |window| {
+            if (window == master) continue;
+            if (!window.is_visible_in(output) or window.floating) continue;
 
-    for (windows.items, 0..) |window, i| {
-        if (i == 0) {
-            window.unbound_move(self.outer_gap, self.outer_gap);
-            window.unbound_resize(master_w, h);
-        } else {
-            window.unbound_move(
-                self.outer_gap + master_w + self.inner_gap,
-                self.outer_gap
-            );
-            window.unbound_resize(stack_w, h);
+            if (!found_stack_top) {
+                found_stack_top = true;
+
+                const master_w = @divFloor(w, 2);
+                const stack_w = w - master_w - self.inner_gap;
+
+                // correct master dimension
+                master.unbound_resize(master_w, h);
+
+                window.unbound_move(
+                    self.outer_gap + master_w + self.inner_gap,
+                    self.outer_gap,
+                );
+                window.unbound_resize(stack_w, h);
+            } else {
+                window.hide();
+            }
         }
     }
 }
